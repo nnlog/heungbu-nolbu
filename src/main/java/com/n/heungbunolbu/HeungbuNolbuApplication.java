2a0ca19515951d922d3e7be9121e3b9e9865fe5c
John <jongseona99@gmail.com>
2022-07-13 18:52:46 +0900

    start

---
+++ src/main/java/com/n/heungbunolbu/HeungbuNolbuApplication.java
@@ -0,0 +1,13 @@
+package com.n.heungbunolbu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeungbuNolbuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeungbuNolbuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/n/heungbunolbu/HeungbuNolbuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.n.heungbunolbu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeungbuNolbuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
